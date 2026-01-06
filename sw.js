8110dd175a92ee1b912c0a65e1bf9a0bd2b91a1b
FJtools <j.fukuda1982@gmail.com>
2026-01-06 15:12:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-const CACHE_NAME = "kanji-practice-v2";
-const ASSETS = [
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./sw.js"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+const CACHE_NAME = "kanji-practice-v3-2";
+const ASSETS = ["./index.html","./style.css","./app.js","./manifest.json","./sw.js"];
+self.addEventListener("install",(event)=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));});
+self.addEventListener("activate",(event)=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));});
+self.addEventListener("fetch",(event)=>{event.respondWith(caches.match(event.request).then(res=>res||fetch(event.request)));});
